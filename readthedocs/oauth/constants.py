4fa83697a043807c5d1278868cd87432a9df949e
Daniel Widerin <daniel.widerin@zumtobelgroup.com>
2015-12-19 02:04:48 +0100

    Add basic GitLab repo sync support

--- readthedocs/oauth/constants.py
+++ readthedocs/oauth/constants.py
@@ -4,9 +4,11 @@ from django.utils.translation import ugettext_lazy as _
 
 
 OAUTH_SOURCE_GITHUB = 'github'
+OAUTH_SOURCE_GITLAB = 'gitlab'
 OAUTH_SOURCE_BITBUCKET = 'bitbucket'
 
 OAUTH_SOURCE = (
     (OAUTH_SOURCE_GITHUB, _('GitHub')),
+    (OAUTH_SOURCE_GITLAB, _('GitLab')),
     (OAUTH_SOURCE_BITBUCKET, _('Bitbucket')),
 )
